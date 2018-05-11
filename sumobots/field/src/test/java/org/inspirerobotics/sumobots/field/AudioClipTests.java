85087f6cb4ae29eda78e39bc9f515ee9c32b2dc0
DevOrc <ncharlton002@gmail.com>
2018-05-10 20:55:14 -0400

    add audio tests

---
+++ sumobots/field/src/test/java/org/inspirerobotics/sumobots/field/AudioClipTests.java
@@ -0,0 +1,26 @@
+package org.inspirerobotics.sumobots.field;
+
+import org.inspirerobotics.sumobots.field.util.AudioEffect;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class AudioClipTests {
+
+	@Test
+	public void testEstopSound() throws IOException {
+		AudioEffect.createAudioStream("estop.wav");
+	}
+
+	@Test
+	public void testStartSound() throws IOException {
+		AudioEffect.createAudioStream("start.wav");
+	}
+
+	@Test(expected = FileNotFoundException.class)
+	public void fakeSoundTest() throws IOException {
+		AudioEffect.createAudioStream("foo.wav");
+	}
+
+}
