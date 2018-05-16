38f329478ee12fbe789d44af0df04e5e324c2fdd
DevOrc <ncharlton002@gmail.com>
2018-05-15 20:47:29 -0400

    Add UnknownMessageTypeException
    - Adds an exception when an unknown message type is used
    - Caught by the message class and logged under severe
    - Also adds a test to verify its thrown.

---
+++ sumobots/library/src/main/java/org/inspirerobotics/sumobots/library/networking/message/UnknownMessageTypeException.java
@@ -0,0 +1,8 @@
+package org.inspirerobotics.sumobots.library.networking.message;
+
+public class UnknownMessageTypeException extends Exception {
+
+	public UnknownMessageTypeException(String message) {
+		super(message);
+	}
+}
