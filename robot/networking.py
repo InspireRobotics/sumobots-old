4b5662d37e116d4b356807467272e6687f833f94
DevOrc <ncharlton002@gmail.com>
2018-05-26 09:21:47 -0400

    add library version and stream terminated message handling for robot code

--- robot/networking.py
+++ robot/networking.py
@@ -4,10 +4,31 @@ import message
 from message import Message
 import json
 
+library_version = "0.2.1"
+name = "pybot"
+
 def bytes_to_json(data):
     # Remove the first two bytes and last byte which are details about the message
     json_string = str(data[2:-1], 'utf-8')
     return json.loads(json_string)
+
+def handle_non_ping_message(type, json, connection):
+    if type == 'LIB_VERSION':
+        version = json['version'];
+        print("Client version:", version)
+
+        if version == library_version:
+            print("Library versions match!")
+        else:
+            print("VERSIONS DO NOT MATCH!!! Robot:", library_version, "\tClient:", version)
+            print("Terminated Robot Connection!")
+            message.stream_terminated().send_from(connection)
+
+    if type == 'STREAM_TERMINATED':
+        print("Client terminated connection!")
+        exit(0)
+
+
 
 def handle_message(json, connection):
     message_type = json['message_type']
@@ -17,7 +38,7 @@ def handle_message(json, connection):
     if(message == 'PING'):
         message.pong_message().send_from(connection)
     else:
-        print("NOT PING")
+        handle_non_ping_message(message_type, json, connection)
 
 def on_data_received(data, connection):
     print('received {!r}'.format(data))
@@ -25,8 +46,6 @@ def on_data_received(data, connection):
 
     handle_message(json, connection)
 
-library_version = "0.2.1"
-name = "pybot"
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,6 +64,7 @@ while True:
     connection, client_address = sock.accept()
 
     message.set_name_message(name).send_from(connection)
+    message.ping_message().send_from(connection)
     message.library_version_message(library_version).send_from(connection)
 
     try:
