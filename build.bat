adeac56a6ab6e695be9969ef41c8b2c890342f0a
DevOrc <ncharlton002@gmail.com>
2018-06-13 17:58:31 -0400

    Add display module to build script

--- build.bat
+++ build.bat
@@ -18,6 +18,9 @@ ren driverstation-0.2.1-jar-with-dependencies.jar driverstation.jar
 cd ../../field/target
 del field-0.2.1.jar
 ren field-0.2.1-jar-with-dependencies.jar field.jar
+cd ../../display/target
+del display-0.2.1.jar
+ren display-0.2.1-jar-with-dependencies.jar display.jar
 
 :: Go back to original folder and clear console
 cd ../../..
