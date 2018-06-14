4718b64a76a3a175befc7ee188d3b44c5ecc67d9
DevOrc <ncharlton002@gmail.com>
2018-06-13 21:22:05 -0400

    Add display backend

---
+++ sumobots/display/src/main/java/org/inspirerobotics/sumobots/display/DisplayBackend.java
@@ -0,0 +1,63 @@
+package org.inspirerobotics.sumobots.display;
+
+import org.inspirerobotics.sumobots.library.InternalLog;
+import org.inspirerobotics.sumobots.library.concurrent.InterThreadMessage;
+import org.inspirerobotics.sumobots.library.concurrent.ThreadChannel;
+
+import java.util.logging.Logger;
+
+public class DisplayBackend extends Thread {
+
+    private static final String threadName = "Display Backend";
+    private final Logger logger = InternalLog.getLogger();
+    private final ThreadChannel threadChannel;
+
+    private boolean running;
+
+    public DisplayBackend(ThreadChannel threadChannel){
+        this.threadChannel = threadChannel;
+        this.setName(threadName);
+    }
+
+    @Override
+    public void run() {
+        logger.info("Starting backend thread");
+        running = true;
+
+        while(running){
+            update();
+        }
+    }
+
+    private void shutdown(){
+        if(!running)
+            logger.severe("The backend thread was shutdown while it was running");
+
+        logger.info("Shutting down backend...");
+        running = false;
+        return;
+    }
+
+    private void update() {
+        handleIncomingMessages();
+    }
+
+    private void handleIncomingMessages() {
+        InterThreadMessage message = null;
+
+        while((message = threadChannel.poll()) != null && running){
+            logger.fine("Received message from frontend: " + formatMessageToString(message));
+            onMessageReceived(message);
+        }
+    }
+
+    private String formatMessageToString(InterThreadMessage message) {
+        return message.getName() + (message.getData() == null ? "":(": " + message.getData()));
+    }
+
+    private void onMessageReceived(InterThreadMessage message) {
+        if(message.getName().equals("shutdown")){
+            shutdown();
+        }
+    }
+}
--- sumobots/display/src/main/java/org/inspirerobotics/sumobots/display/DisplayFrontend.java
+++ sumobots/display/src/main/java/org/inspirerobotics/sumobots/display/DisplayFrontend.java
@@ -1,20 +1,30 @@
 package org.inspirerobotics.sumobots.display;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import org.inspirerobotics.sumobots.library.InternalLog;
 import org.inspirerobotics.sumobots.library.Resources;
+import org.inspirerobotics.sumobots.library.concurrent.InterThreadMessage;
+import org.inspirerobotics.sumobots.library.concurrent.ThreadChannel;
 
 import java.util.logging.Logger;
 
 public class DisplayFrontend extends Application {
 
-    private Logger logger = InternalLog.getLogger();
+    private static final String threadName = "Display Frontend";
+    private final Logger logger = InternalLog.getLogger();
+    private final ThreadChannel threadChannel = new ThreadChannel();
+    private final DisplayBackend displayBackend = new DisplayBackend(threadChannel.createPair());
     private Stage stage;
 
     @Override
     public void start(Stage primaryStage) {
+        Thread.currentThread().setName(threadName);
         logger.info("Initializing the display...");
+        this.displayBackend.start();
         this.stage = primaryStage;
 
         initStage(stage);
@@ -23,7 +33,13 @@ public class DisplayFrontend extends Application {
 
     private void initStage(Stage stage) {
         stage.setTitle("FMS Display System: " + Resources.LIBRARY_VERSION);
+        stage.setOnCloseRequest(new FrontendShutdownHandler(this));
         stage.show();
+    }
+
+    public void shutdown(){
+        Platform.exit();
+        threadChannel.add(new InterThreadMessage("shutdown"));
     }
 
     public static void main(String[] args) {
@@ -31,3 +47,17 @@ public class DisplayFrontend extends Application {
     }
 
 }
+class FrontendShutdownHandler implements EventHandler<WindowEvent> {
+
+    private DisplayFrontend displayFrontend;
+
+    public FrontendShutdownHandler(DisplayFrontend displayFrontend){
+        this.displayFrontend = displayFrontend;
+    }
+
+    @Override
+    public void handle(WindowEvent event) {
+        displayFrontend.shutdown();
+    }
+}
+
