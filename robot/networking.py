636da6b6ec90f082c5181dfa8409eaf9fd23133e
DevOrc <ncharlton002@gmail.com>
2018-06-23 09:13:22 -0400

    Fix robot code

--- robot/networking.py
+++ robot/networking.py
@@ -3,7 +3,7 @@ import sys
 import message
 from message import Message
 import json
-import pwm_test
+# import pwm_test
 
 library_version = "0.2.1"
 name = "pybot"
@@ -19,7 +19,7 @@ def bytes_to_json(data):
     return json.loads(json_string)
 
 
-def handle_non_ping_message(type, json, connection):
+def handle_non_simple_message(type, json, connection):
     if type == 'LIB_VERSION':
         version = json['version']
         print("Client version:", version)
@@ -30,10 +30,6 @@ def handle_non_ping_message(type, json, connection):
             print("VERSIONS DO NOT MATCH!!! Robot:", library_version, "\tClient:", version)
             print("Terminated Robot Connection!")
             message.stream_terminated().send_from(connection)
-
-    if type == 'STREAM_TERMINATED':
-        print("Client terminated connection!")
-        exit(0)
 
     if type == "JOY_UPDATE":
         handle_joystick(json)
@@ -58,23 +54,28 @@ def handle_joystick(json):
         leftX = json["Left X Axis"]
 
     print("Joystick Values: RX: {}, RY: {}, LX: {}, LY: {}".format(rightX, rightY, leftX, leftY))
-    pwm_test.set_speed(14, float(leftY), 0)
-    pwm_test.set_speed(15, float(leftY), 0)
-    pwm_test.set_speed(18, float(leftY), 0)
+    # pwm_test.set_speed(14, float(leftY), 0)
+    # pwm_test.set_speed(15, float(leftY), 0)
+    # pwm_test.set_speed(18, float(leftY), 0)
     
-    pwm_test.set_speed(23, float(rightY), 0)
-    pwm_test.set_speed(24, float(rightY), 0)
-    pwm_test.set_speed(25, float(rightY), 0)
+    # pwm_test.set_speed(23, float(rightY), 0)
+    # pwm_test.set_speed(24, float(rightY), 0)
+    # pwm_test.set_speed(25, float(rightY), 0)
 
 def handle_message(json, connection):
     message_type = json['message_type']
 
     print("Message Type:", message_type)
 
-    if(message == 'PING'):
+    if(message_type == 'PING'):
         message.pong_message().send_from(connection)
+    elif(message_type == 'STREAM_TERMINATED'):
+        print("The client has terminated the stream...")
+        return True
     else:
-        handle_non_ping_message(message_type, json, connection)
+        handle_non_simple_message(message_type, json, connection)
+
+    return False
 
 def on_data_received(data, connection):
     print('received {!r}'.format(data))
@@ -88,40 +89,44 @@ def on_data_received(data, connection):
 
     if json is None:
         print("Couldn't format JSON:", data)
-        return
+        return False
 
-    handle_message(json, connection)
+    return handle_message(json, connection)
 
+def run():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Bind the socket to the port
+    server_address = ('0.0.0.0', 10489)
+    print('starting up on {} port {}'.format(*server_address))
+    sock.bind(server_address)
 
-# Bind the socket to the port
-server_address = ('0.0.0.0', 10489)
-print('starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
+    # Listen for incoming connections
+    sock.listen(1)
 
-# Listen for incoming connections
-sock.listen(1)
+    while True:
+        # Wait for a connection
+        print('waiting for a connection')
+        connection, client_address = sock.accept()
 
-while True:
-    # Wait for a connection
-    print('waiting for a connection')
-    connection, client_address = sock.accept()
+        message.set_name_message(name).send_from(connection)
+        message.ping_message().send_from(connection)
+        message.library_version_message(library_version).send_from(connection)
 
-    message.set_name_message(name).send_from(connection)
-    message.ping_message().send_from(connection)
-    message.library_version_message(library_version).send_from(connection)
+        try:
+            print('connection from', client_address)
 
-    try:
-        print('connection from', client_address)
+            # Receive the data in small chunks and retransmit it
+            while True:
+                data = connection.recv(512)
+                if on_data_received(data, connection):
+                    print("Closing connection!")
+                    break
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(512)
-            on_data_received(data, connection)
+        finally:
+            # Clean up the connection
+            connection.close()
 
-    finally:
-        # Clean up the connection
-        connection.close()
-
+# Run the robot code
+run()
